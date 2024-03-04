e4d7f5db22418f29d92dfe571be698b066486c00
faradox <felix@okio.ai>
2024-03-04 12:49:53 +0100

    chore: prepare release 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 if __name__ == "__main__":
     setup(
         name="nendo-plugin-library-postgresdb",
-        version="0.1.4",
+        version="0.1.5",
         description="Nendo PostgresDB library plugin",
         author="Okio Team <team@okio.ai>",
     )
